2c99e0a487c212194cf9038e67f9befe985e4b96
eycha <silvlexonst@gmail.com>
2022-12-05 16:59:56 +0900

    interface

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor={"teal"} />
+      <Circle bgColor={"tomato"} />
+      <Circle bgColor={"teal"} />
+    </div>
+  );
+}
+
+export default App;
