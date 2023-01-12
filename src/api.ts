770cd59bdd164dd92db8c30821372bbdf6c36408
eycha <silvlexonst@gmail.com>
2023-01-12 18:37:44 +0900

    api

--- src/api.ts
+++ src/api.ts
@@ -15,7 +15,7 @@ export async function fetchCoinTickers(coinId: string) {
 
 export async function fetchCoinHistory(coinId: string) {
   const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7;
+  const startDate = endDate - 60 * 60 * 7;
   return await (
     await fetch(
       `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
