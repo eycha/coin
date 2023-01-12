c2f68e54000d8a42d7dc9109fcc6af8502f23a7c
차은엽 <eycha@cha-eun-yeob-ui-MacBookAir.local>
2023-01-13 08:08:45 +0900

    fix: not using library  delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
-import {ReactQueryDevtools} from "react-query/devtools";
-import {darkTheme, lightTheme} from "./theme";
-import {ThemeProvider} from "styled-components";
-import {useState} from "react";
-import {useRecoilValue} from "recoil";
-import {isDarkAtom} from "./atoms";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { darkTheme, lightTheme } from "./theme";
+import { ThemeProvider } from "styled-components";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400&display=swap');
